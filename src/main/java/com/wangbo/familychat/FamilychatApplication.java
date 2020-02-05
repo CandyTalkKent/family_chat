1b907124823b0efe7d407678c9a76d71ddd5aac6
manchesterKent <609376784@qq.com>
2020-02-05 15:13:42 +0800

    init project

---
+++ src/main/java/com/wangbo/familychat/FamilychatApplication.java
@@ -0,0 +1,13 @@
+package com.wangbo.familychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilychatApplication.class, args);
+	}
+
+}
